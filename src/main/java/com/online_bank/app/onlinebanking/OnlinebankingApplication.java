706e1995ef4fe7dc293661cc766ac4e9b4a66b1d
sarfaroj <sarfaroj95@gmail.com>
2026-03-27 12:39:06 +0530

    Add entity createAt and remove old file
    Add entity createAt and remove old file

--- src/main/java/com/online_bank/app/onlinebanking/OnlinebankingApplication.java
+++ src/main/java/com/online_bank/app/onlinebanking/OnlinebankingApplication.java
@@ -2,8 +2,10 @@ package com.online_bank.app.onlinebanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OnlinebankingApplication {
 
 	public static void main(String[] args) {
